b45dc4e2ee85f203e7485f487348bf593f7b83d5
issa99-lab <issanana99@gmail.com>
2024-07-20 18:07:23 +0300

    Handler

---
+++ test/Invariant tests/Handler.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GNU General Public License v3.0
+pragma solidity 0.8.20;
+
+import {TSwapPool} from "../../src/TSwapPool.sol";
+import {ERC20Mock} from "lib/node_modules/@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {PoolFactory} from "../../src/PoolFactory.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract Handler is Test {
+    TSwapPool pool;
+
+    ERC20Mock token;
+    ERC20Mock weth;
+
+    address liquidityProvider = makeAddr("lp");
+    address swapper = makeAddr("swp");
+
+    int256 startingX;
+    int256 startingY;
+    int256 expectedDeltaX;
+    int256 expectedDeltaY;
+    int256 actualDeltaX;
+    int256 actualDeltaY;
+
+    constructor(TSwapPool _pool) {
+        pool = _pool;
+        weth = ERC20Mock(pool.getWeth());
+        token = ERC20Mock(pool.getPoolToken());
+    }
+
+    function deposit(uint256 wethAmount) public {
+        wethAmount = bound(wethAmount, 0, type(uint64).max);
+
+        startingY = int256(weth.balanceOf(address(this)));
+        startingX = int256(token.balanceOf(address(this)));
+
+        expectedDeltaY = int256(wethAmount);
+        expectedDeltaX = int256(
+            pool.getPoolTokensToDepositBasedOnWeth(wethAmount)
+        );
+        //pool.deposit(wethAmount, 0, wethAmount,)
+
+        vm.startPrank(liquidityProvider);
+        weth.mint(liquidityProvider, wethAmount);
+        token.mint(liquidityProvider, uint256(expectedDeltaX));
+
+        weth.approve(address(pool), type(uint256).max);
+        token.approve(address(pool), type(uint256).max);
+
+        pool.deposit(
+            wethAmount,
+            0,
+            uint256(expectedDeltaX),
+            uint64(block.timestamp)
+        );
+
+        //actual
+        uint256 endingY = weth.balanceOf(address(this));
+        uint256 endingX = token.balanceOf(address(this));
+        actualDeltaY = int256(endingY) - int256(startingY);
+        actualDeltaX = int256(endingX) - int256(startingX);
+    }
+}
--- test/Invariant tests/Invariant.t.sol
+++ test/Invariant tests/Invariant.t.sol
@@ -7,4 +7,31 @@ import {PoolFactory} from "../../src/PoolFactory.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
-contract InvariantTest is StdInvariant, Test {}
+contract InvariantTest is StdInvariant, Test {
+    ERC20Mock token;
+    ERC20Mock weth;
+
+    PoolFactory factory;
+    TSwapPool pool;
+
+    int256 constant STARTING_X = 100e18; //tokens
+    int256 constant STARTING_Y = 50e18; // weth
+
+    function setUp() public {
+        token = new ERC20Mock();
+        weth = new ERC20Mock();
+
+        token.mint(address(this), uint256(STARTING_X));
+        weth.mint(address(this), uint256(STARTING_Y));
+
+        token.approve(address(pool), type(uint256).max);
+        weth.approve(address(pool), type(uint256).max);
+
+        pool.deposit(
+            uint256(STARTING_Y),
+            uint256(STARTING_Y),
+            uint256(STARTING_X),
+            uint64(block.timestamp)
+        );
+    }
+}
